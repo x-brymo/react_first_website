c592f64dfdf5da5cedbc868fa3bbc4a0d16c6070
Mahmoud Hafez Eltarqi <98850036+x-brymo@users.noreply.github.com>
2023-08-21 10:24:22 +0300

    update

---
+++ src/feature/login/form/form.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Container }from 'react-bootstrap'
+ const FormLogin=()=>{
+    return (
+        <Container></Container>
+    );
+}
+export default FormLogin;
---
+++ src/feature/view/home/home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from '../home/header'
+const Home = () => {
+    return (
+        <Header />
+    );
+}
+export default Home;
